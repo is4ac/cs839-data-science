66f0ca125bd0bac94721bab85c404504da61038f
Hoai Nguyen <ms.hoainguyen@gmail.com>
2018-02-27 18:54:10 -0600

    Add cross validation logic.

--- pipeline.py
+++ pipeline.py
@@ -1,4 +1,8 @@
 import csv
+import numpy
+import os
+import tempfile
+import uuid
 from sklearn import svm
 from sklearn import tree
 from sklearn.ensemble import RandomForestClassifier
@@ -9,7 +13,8 @@ from sklearn.metrics import recall_score
 from featured_data_generator import OTHER_FEATURES
 from featured_data_generator import TRAIN_CSV
 from featured_data_generator import TEST_CSV
-import numpy as np
+from featured_data_generator import createDevAndTestFileSet
+from featured_data_generator import extractAndCreateCSV
 
 def get_feature_and_label_from_file(input_csv_file):
     """Reads a csv file and produce feature vectors and label.
@@ -114,6 +119,115 @@ def convert_to_binary(list):
 
     return new_list
 
+def chunkIt(seq, num):
+    """Divides list 'seq' into 'num' rougly equal parts. """
+    avg = len(seq) / float(num)
+    out = []
+    last = 0.0
+    while last < len(seq):
+        out.append(seq[int(last):int(last + avg)])
+        last += avg
+
+    return out
+
+def create_tmp_files():
+    """Returns a unique, non-existing tmp train and test file names."""
+    tmp_file = os.path.join(tempfile.gettempdir(), str(uuid.uuid4()))
+    tmp_train = tmp_file + '_train.csv'
+    tmp_test = tmp_file + '_test.csv'
+    while os.path.isfile(tmp_train) or os.path.isfile(tmp_test):
+        tmp_file = os.path.join(tempfile.gettempdir(), str(uuid.uuid4()))
+        tmp_train = tmp_file + '_train.csv'
+        tmp_test = tmp_file + '_test.csv'
+    return tmp_train, tmp_test
+
+def compute_average_P_and_R(folds, clf):
+    """Returns average precision and recall scores given a set of file folds."""
+    precisions = []
+    recalls = []
+    for i in range (0, 5):
+        print('Round %s' % (i + 1))
+        test_set = folds[i]
+        train_set = []
+        for j in range (0, 5):
+            if (j != i):
+                train_set += folds[j]
+        train_csv, test_csv = create_tmp_files()
+        extractAndCreateCSV(train_set, train_csv)
+        extractAndCreateCSV(test_set, test_csv)
+        train_data, train_label = get_feature_and_label_from_file(train_csv)
+        test_data, test_label = get_feature_and_label_from_file(test_csv)
+        clf.fit(train_data, train_label)
+        test_predict = clf.predict(test_data)
+        test_predict = convert_to_binary(test_predict)
+        precisions.append(precision_score(test_label, test_predict))
+        recalls.append(recall_score(test_label, test_predict))
+        # clean up temporary files.
+        os.remove(train_csv)
+        os.remove(test_csv)
+    avg_P = numpy.mean(precisions)
+    avg_R = numpy.mean(recalls)
+    print('AVG precision = %s' % avg_P)
+    print('AVG recall = %s' % avg_R)
+    return avg_P, avg_R
+
+def cross_validation():
+    """Performs cross validation on certain classifier 'clf'.
+    Returns average precision and recall scores.
+    """
+    dev_file_set, test_file_set = createDevAndTestFileSet()
+    folds = chunkIt(dev_file_set, 5)
+    print('Cross Validation for DecisionTree')
+    avg_P, avg_R = compute_average_P_and_R(folds, tree.DecisionTreeClassifier())
+
+    max_avg_P = avg_P
+    max_avg_R = avg_R
+    highest_P_clf = 'DecisionTree'
+    highest_R_clf = 'DecisionTree'
+
+    print('Cross Validation for Random Forest')
+    avg_P, avg_R = compute_average_P_and_R(folds,
+                                           RandomForestClassifier(n_estimators=10,
+                                                                  max_depth=50,
+                                                                  random_state=0))
+    if (max_avg_P < avg_P):
+        max_avg_P = avg_P
+        highest_P_clf = 'RandomForest'
+    if (max_avg_R < avg_R):
+        max_avg_R = avg_R
+        highest_R_clf = 'RandomForest'
+
+    print('Cross Validation for SVC')
+    avg_P, avg_R = compute_average_P_and_R(folds, svm.SVC())
+    if (max_avg_P < avg_P):
+        max_avg_P = avg_P
+        highest_P_clf = 'SVC'
+    if (max_avg_R < avg_R):
+        max_avg_R = avg_R
+        highest_R_clf = 'SVC'
+
+    print('Cross Validation for LinearRegression')
+    avg_P, avg_R = compute_average_P_and_R(folds, LinearRegression())
+    if (max_avg_P < avg_P):
+        max_avg_P = avg_P
+        highest_P_clf = 'LinearRegression'
+    if (max_avg_R < avg_R):
+        max_avg_R = avg_R
+        highest_R_clf = 'LinearRegression'
+
+    print('Cross Validation for LogisticRegression')
+    avg_P, avg_R = compute_average_P_and_R(folds, LogisticRegression())
+    if (max_avg_P < avg_P):
+        max_avg_P = avg_P
+        highest_P_clf = 'LogisticRegression'
+    if (max_avg_R < avg_R):
+        max_avg_R = avg_R
+        highest_R_clf = 'LogisticRegression'
+
+    print('%s has the highest precision score %s' % (highest_P_clf, max_avg_P))
+    print('%s has the highest recall score %s' % (highest_R_clf, max_avg_R))
+
+
 def main():
     dev_data, dev_labels, test_data, test_labels = get_dev_and_test_data()
     # Fit, predict, and compute precision & recall scores for several classifiers.
@@ -175,4 +289,5 @@ def main():
     print('LogisticRegression Recall score: {0:0.2f}'.format(recall))
 
 if __name__ == "__main__":
-    main()
+    # main()
+    cross_validation()
