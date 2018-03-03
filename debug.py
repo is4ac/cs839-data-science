31524bc0f7c83d387b8145527409e52a16363d1c
Hoai Nguyen <ms.hoainguyen@gmail.com>
2018-03-03 17:06:30 -0600

    Add a script for debugging

---
+++ debug.py
@@ -0,0 +1,25 @@
+from collections import Counter
+import operator
+import csv
+
+words_false_positive = []
+words_false_negative = []
+
+with open('stage1_docs/Data/results.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        if row['class_label'] == '0':
+            words_false_positive.append(row['string'])
+        else:
+            words_false_negative.append(row['string'])
+
+counts_positive = Counter(words_false_positive)
+counts_negative = Counter(words_false_negative)
+#print(counts_positive)
+#print(counts_negative)
+sorted_counts = sorted(counts_negative.items(), key=operator.itemgetter(1))
+whitelist = ''
+for (key, value) in sorted_counts:
+    if value >= 3:
+        whitelist = whitelist + ("'%s'," % key)
+print whitelist
